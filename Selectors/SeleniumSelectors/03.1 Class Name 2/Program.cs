e4c48b87b83763d345693b074e6d42a5b3cb22ad
Ramon <ramongreyes@icloud.com>
2018-11-02 16:50:56 -0500

    'updating1650hrs'

--- Selectors/SeleniumSelectors/03.1 Class Name 2/Program.cs
+++ Selectors/SeleniumSelectors/03.1 Class Name 2/Program.cs
@@ -10,8 +10,10 @@ class ChkClass
 {
     static void Main()
     {
-        string url = "https://promosuat.icgrouplp.com/CA/en/instantwin/index?id=MS_FW_UAT_NOV_CA_18&bruid=837FA97A9C2C85B7046A2163FFFFFFFF&Hash=f1a3c95b4e4ce97a126ce70c6609c1246febff8b30250a6ec6ab5133717b546a";
-        string className = "btn btn-long";
+        string url = "https://promosuat.icgrouplp.com/CA/en/handshake?uid=837FA97A9C2C85B7046A2163FFFFFFFF&token=6dbf9d97-9515-4780-a139-508e24e4c865&gpid=MS_FW_UAT_NOV_CA_18-20&c=303bff478b06a256601dfeddd6d0386fe313195c3aeeab2bcec81f80b2baeab6";
+        //string className = "btn btn-long";
+
+        string csselector = "body > div.flip.flip - landing > div.play > button";
 
         IWebDriver driver = new ChromeDriver();
 
@@ -19,7 +21,8 @@ class ChkClass
 
         
 
-        IWebElement element = driver.FindElement(By.ClassName(className));
+        //IWebElement element = driver.FindElement(By.ClassName(className));
+        IWebElement element = driver.FindElement(By.CssSelector(csselector));
 
         Console.WriteLine(element.Text);
 
@@ -27,12 +30,14 @@ class ChkClass
 
         if (element.Displayed)
         {
-            message.GreenMessage("Yes, An Element ID '" + className + "' Has Been Found");
+            //message.GreenMessage("Yes, An Element ID '" + className + "' Has Been Found");
+            message.GreenMessage("Yes, An Element ID '" + csselector + "' Has Been Found");
 
         }
         else
         {
-            message.RedMessage("No,  Element " + className + " Exist");
+            //message.RedMessage("No,  Element " + className + " Exist");
+            message.RedMessage("No,  Element " + csselector + " Exist");
         }
         driver.Quit();
     }
---
+++ Selectors/SeleniumSelectors/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("61ed3252-b6c8-447e-883d-670783646830")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
